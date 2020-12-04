3c759ce2c1ce84adbfc6cd4570b148eda2cd9df0
anikey <anikey.m@gmail.com>
2020-12-05 00:08:06 +0400

    Add date range filter for order admin page.

--- order/admin.py
+++ order/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from rangefilter.filter import DateTimeRangeFilter
 
 from . import models
 
@@ -7,3 +8,8 @@ from . import models
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'created', 'changed', 'total', 'contractor', 'brief_text')
     search_fields = ('total', 'contractor', 'text')
+    list_filter = (
+        ('created', DateTimeRangeFilter),
+        ('changed', DateTimeRangeFilter),
+    )
+
